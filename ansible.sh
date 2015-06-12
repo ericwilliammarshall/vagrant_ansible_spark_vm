bdb41c820556a6d06ebcdd9a18937ee75a0d23f0
ericwilliammarshall <eric.marshall@mac.com>
2015-06-12 10:52:35 -0400

    added centos gui packages, changed passwords

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=.vagrant/machines/default/virtualbox/private_key -u vagrant provisioning/site.yml --sudo --limit @/Users/eric/site.retry
+ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=.vagrant/machines/default/virtualbox/private_key -u vagrant provisioning/site.yml --sudo --limit @/Users/eric/site.retry #--start-at-task=
